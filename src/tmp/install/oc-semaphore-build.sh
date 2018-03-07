2634abc6b5ca052313e1124023442b6051de9fdc
Joern Ott <joern.ott@ott-consult.de>
2018-03-07 16:58:18 +0000

    Fix checkouts

--- src/tmp/install/oc-semaphore-build.sh
+++ src/tmp/install/oc-semaphore-build.sh
@@ -16,11 +16,11 @@ install_software git glibc-static gcc make
 create_user_and_group
 install_go
 mkdir -p "${APP_HOME}/go/src/github.com/joernott" "${APP_HOME}/go/bin" "${APP_HOME}/go/pkg"
-chown -R ${APP_USER}:${APP_GROUP} "${APP_HOME}/go"
 cd "${APP_HOME}/go/src/github.com/joernott"
-gosu ${APP_USER}:${APP_GROUP} git clone --recursive https://github.com/joernott/semaphore.git
+git clone --recursive https://github.com/joernott/semaphore.git
 cd semaphore
-gosu ${APP_USER}:${APP_GROUP} go get ./... github.com/cespare/reflex github.com/jteeuwen/go-bindata/... github.com/mitchellh/gox
+go get ./... github.com/cespare/reflex github.com/jteeuwen/go-bindata/... github.com/mitchellh/gox
+chown -R ${APP_USER}:${APP_GROUP} "${APP_HOME}/go"
 npm install -g async
 npm install -g nodemon pug-cli less
 mkdir /output
